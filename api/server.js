61af03d96eaf5fd501fbb6cd650aeffa698f3107
Austin Walela <austinmasibo@gmail.com>
2020-03-17 10:02:31 +0300

    added GET query and endpoint

--- api/server.js
+++ api/server.js
@@ -1,16 +1,34 @@
 const express = require('express')
 const morgan = require('morgan')
 
-const accountsRouter = require('./accounts/accountsRouter')
-
+const db = require('../data/dbConfig')
 const server = express()
-server.use(morgan('dev'))
-server.use(express.json())
 
-server.use('api/accounts', accountsRouter)
+server.use(express.json())
+server.use(morgan('dev'))
+
+function getAll() {
+  return db.select('id', 'name', 'budget').from('accounts')
+}
+
+function getById(id) {
+  return db('posts')
+    .where({ id: id })
+    .select('id', 'name', 'budget')
+}
 
 server.get('/', (_, res) => {
   res.send('<h1>Accounts API with knex</h1>')
+})
+
+server.get('/api/accounts/', async (req, res) => {
+  try {
+    const data = await getAll()
+    res.status(200).json(data)
+  } catch (err) {
+    console.log(err)
+    res.status(500).json({ message: 'Error getting accounts' })
+  }
 })
 
 server.all('*', (req, res) => {
